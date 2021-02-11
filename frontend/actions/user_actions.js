56396a28e4504eeb4cbd599cf81765f0b69689a1
Mac Mini <ryan@cygaq.com>
2021-02-10 19:36:10 -0800

    fixed react app

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,39 @@
+import * as UserApiUtil from '../utils/user_api_util';
+
+
+export const RECEIVE_USER = 'RECEIVE_USER';
+export const RECEIVE_USER_ERRORS = "RECEIVE_USER_ERRORS";
+export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
+
+export const receiveUserErrors = errors => {
+    return{
+        type: RECEIVE_USER_ERRORS,
+        errors
+    }
+}
+
+const receiveUser = userId => {
+    return{
+        type: RECEIVE_USER,
+        userId
+    }
+};
+
+const receiveAllUsers = users => {
+    return{
+        type: RECEIVE_ALL_USERS,
+        users
+
+    }
+}
+
+
+export const fetchUser = userId => dispatch => {
+    return UserApiUtil.fetchUser(userId)
+        .then(pin => dispatch(receiveUser(pin)))
+}
+
+export const fetchUsers = () => dispatch => {
+    return UserApiUtil.fetchUsers()
+        .then(users => dispatch(receiveAllUsers(users)))
+}
