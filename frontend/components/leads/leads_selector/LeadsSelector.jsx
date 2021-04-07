06c3335d3fe05595eaccfda6db54df5dda028b45
Mac Mini <ryan@cygaq.com>
2021-04-07 09:59:25 -0700

    soft redesign of color scheme and borders

--- frontend/components/leads/leads_selector/LeadsSelector.jsx
+++ frontend/components/leads/leads_selector/LeadsSelector.jsx
@@ -14,22 +14,22 @@ function LeadsSelector() {
     return (
         <div className='flex-table table-header'>
                     <div className='uncontacted' data-value='Uncontacted' id={ctx.selectPipeline === 'Uncontacted' ? 'selected' : null } onClick={toggleLeadType}>
-                    Uncontacted
+                    UNCONTACTED
                 </div>
                     <div className='contacted' data-value='Contacted' id={ctx.selectPipeline === 'Contacted' ? 'selected' : null } onClick={toggleLeadType}>
-                    Contacted
+                    CONTACTED
                 </div>
                     <div className='counter-received' data-value='Counter Received' id={ctx.selectPipeline === 'Counter Received' ? 'selected' : null }  onClick={toggleLeadType}>
-                    Counter Received
+                    COUNTER RECEIVED
                 </div>
                     <div className='counter-responded' data-value='Counter Responded' id={ctx.selectPipeline === 'Counter Responded' ? 'selected' : null } onClick={toggleLeadType}>
-                    Counter Responded                    
+                    COUNTER RESPONDED                   
                 </div>
                     <div className='under-contract' data-value='Under Contract' id={ctx.selectPipeline === 'Under Contract' ? 'selected' : null } onClick={toggleLeadType}>
-                    Under Contract                     
+                    UNDER CONTRACT                  
                 </div>
                 <div className='closed' data-value='Closed' id={ctx.selectPipeline === 'Closed' ? 'selected' : null } onClick={toggleLeadType}>
-                    Closed                     
+                    CLOSED                     
                 </div>
                 <div className='watched' data-value='Watched' id={ctx.selectPipeline === 'Watched' ? 'selected' : null } onClick={toggleLeadType}>
                     <span className="iconify" data-icon="vscode-icons:file-type-bolt" data-inline="false"></span>
